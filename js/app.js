062772b3f7b20941ee1a91e294b7dac758d6d6c5
Brian Hurst <brian.hurst@gsa.gov>
2026-03-26 12:45:25 -0400

    Use pathPrefix

--- js/app.js
+++ js/app.js
@@ -7,7 +7,25 @@ function initializeSearchFeatures() {
   initializeResourceSearch();
   initializeCaseSearch();
 
-  if (window.location.pathname === "/" || window.location.pathname === "/index.html") {
+  const rawBaseHref = document.querySelector("base")?.getAttribute("href") || "/";
+  let pathPrefix = "/";
+  try {
+    pathPrefix = new URL(rawBaseHref, window.location.origin).pathname;
+  } catch (error) {
+    pathPrefix = rawBaseHref;
+  }
+
+  if (!pathPrefix.startsWith("/")) {
+    pathPrefix = `/${pathPrefix}`;
+  }
+
+  const normalizedPrefix = pathPrefix.replace(/\/+$/, "") || "/";
+  const homePaths =
+    normalizedPrefix === "/"
+      ? ["/", "/index.html"]
+      : [normalizedPrefix, `${normalizedPrefix}/`, `${normalizedPrefix}/index.html`];
+
+  if (homePaths.includes(window.location.pathname)) {
     console.log("Everything you need to run your compliant government website: http://www.cloud.gov/pages/");
   }
 }
