7760b28accc3b225e4a67fc9d9a44c49b5b1504b
Brian Hurst <brian.hurst@gsa.gov>
2026-03-26 12:39:04 -0400

    For funsies

--- js/app.js
+++ js/app.js
@@ -6,6 +6,10 @@ const { initializeResourceSearch } = require("./search/resource-search");
 function initializeSearchFeatures() {
   initializeResourceSearch();
   initializeCaseSearch();
+
+  if (window.location.pathname === "/" || window.location.pathname === "/index.html") {
+    console.log("Everything you need to run your compliant government website: http://www.cloud.gov/pages/");
+  }
 }
 
 if (document.readyState === "loading") {
